a530113be6007c5dbba063240ac5a09e9ff386b7
raccoon-creater <secracc460192@gmail.com>
2020-12-21 23:00:37 +0900

    header・footer関数化

---
+++ footer.php
@@ -0,0 +1,11 @@
+    <footer>
+        <ul class="footer-menu">
+            <li><a href="#">home</a> |</li>
+            <li><a href="#">POST</a> |</li>
+            <li><a href="#">LIST</a> |</li>
+            <li><a href="#">SIMULATION</a></li>
+        </ul>
+        <p>©All rights reserved by Siberian Nekosky.</p>
+    </footer>
+</body>
+</html>
